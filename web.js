023abd4b65dbdb51a6ebcf6521826c9b665786a2
Ron van Teylingen <ron@txi.nl>
2013-07-23 15:09:51 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -2,9 +2,12 @@ var express = require('express');
 var app = express();
 app.use(express.logger());
 
+var htmlfromfile = fs.readFileSync("index.html"); 
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
-});
+//  response.send('Hello World 2!');
+  response.send(htmlfromfile.toString());
+});  
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
