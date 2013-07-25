4e5f239f8468bdb97d54a6b8f6d7a793f91ff775
Ron van Teylingen <ron@txi.nl>
2013-07-25 09:23:53 +0000

    tested locally

--- web.js
+++ web.js
@@ -1,15 +1,16 @@
 var express = require('express');
 var app = express();
+var fs = require('fs');
 app.use(express.logger());
 
-var htmlfromfile = fs.readFileSync("index.html"); 
+var htmlfromfile = fs.readFileSync("index.html");
 
 app.get('/', function(request, response) {
 //  response.send('Hello World 2!');
-  response.send(htmlfromfile.toString());
-});  
+    response.send(htmlfromfile.toString());
+});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+    console.log("Listening on " + port);
 });
